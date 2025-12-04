1a32d53c596afa092c17e1f7183e30ca218323a9
uconnwbball <jd5208675309@gmail.com>
2025-12-03 22:07:22 -0700

    3 Dec 25 - Feat: filter functionality in Destinations page

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,14 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
+import data from "../data.json"
+
 export default function Crew(){
+    const [searchParams] = useSearchParams()
+    const crewFilter = searchParams.get("name") || "hurley"
+    const crewData = data.crew
+    console.log("crewData", crewData)
+    const filteredCrew = crewData.filter(member=>{
+        console.log("member", member)
+        return crewFilter === member.name})
+   console.log("filteredCrew", filteredCrew)
+
+//T2D - Need to fix filtration logic to return the filteredCrew member
+
     return (
         <div className="crew-div">
             <p>02 Meet your crew</p>
             <nav className = "crew-nav">
-                <Link to="/crew/hurley" className="crew-link"></Link>
-                <Link to="/crew/shuttleworth" className="crew-link"></Link>
-                <Link to="/crew/glover" className="crew-link"></Link>
-                <Link to="/crew/ansari" className="crew-link"></Link>
+                <Link to="?name=hurley" className="crew-link"></Link>
+                <Link to="?name=shuttleworth" className="crew-link"></Link>
+                <Link to="?name=glover" className="crew-link"></Link>
+                <Link to="?name=ansari" className="crew-link"></Link>
             </nav>
+            <h2></h2>
         </div>
     )
 }
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,35 +1,41 @@
-import { Link, NavLink, useSearchParams } from "react-router-dom"
+import { NavLink, useSearchParams } from "react-router-dom"
 import data from "../data.json"
 //the default page for destinations is the Moon 
 export default function Destination(){
 
 const [searchParams] = useSearchParams()
-const destinationFilter = searchParams.get("destination")
-console.log("destinationFilter", destinationFilter)
+console.log(searchParams)
+const destinationFilter = searchParams.get("destination") || "moon"
 const destinationsData = data.destinations
-console.log("destinationsData", destinationsData)
+
 const filteredDestination = destinationsData.filter(destination=>{
-    console.log("destination", destination)
-    return destination.name.toLowerCase() === destinationFilter})
-console.log("filteredDestination", filteredDestination)
+    return destinationFilter === destination.name.toLowerCase()})
+
 //fix the active links
 const activeLink={
-    color: "#D0D6F9"
+    color: "#FFFFFF"
 }
     return (
         <div className="destination-div">
             <h2>01 <span className="white-text">PICK YOUR DESTINATION</span></h2>
             <nav className="destination-nav">
                 <NavLink to="?destination=moon" style={({isActive})=>isActive? activeLink: null} className="light-blue-text">MOON</NavLink>
-                <NavLink to="?destination=mars">MARS</NavLink>
-                <NavLink to="/destination/europa">EUROPA</NavLink>
-                <NavLink to="/destination/titan">TITAN</NavLink>
+                <NavLink to="?destination=mars" style={({isActive})=>isActive? activeLink: null} className="light-blue-text">MARS</NavLink>
+                <NavLink to="?destination=europa" style={({isActive})=>isActive? activeLink: null} className="light-blue-text">EUROPA</NavLink>
+                <NavLink to="?destination=titan" style={({isActive})=>isActive? activeLink: null} className="light-blue-text">TITAN</NavLink>
             </nav>
-            <h2>MOON</h2>
-            <p>To be filled in...</p>
+            <h2 className="white-text">{destinationFilter? filteredDestination[0].name: null}</h2>
+            <p className="light-blue-text">{destinationFilter? filteredDestination[0].description: null}</p>
+            <img src={`${destinationFilter? filteredDestination[0].images.png: null}`}/>
             <div>
-                <p>AVG. DISTANCE</p>
-                <p>EST. TRAVEL TIME</p>
+                <div>
+                    <p className="light-blue-text">AVG. DISTANCE</p>
+                    <p className="white-text">{destinationFilter? filteredDestination[0].distance: null}</p>
+                </div>
+                <div>
+                    <p className="light-blue-text">EST. TRAVEL TIME</p>
+                    <p className="white-text"> {destinationFilter? filteredDestination[0].travel: null}</p>
+                </div>
             </div>
         </div>
        
