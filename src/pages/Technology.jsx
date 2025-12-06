bbcf2bc5eed78275420346c657bd5ab95490efca
uconnwbball <jd5208675309@gmail.com>
2025-12-05 22:07:56 -0700

    5 Dec 25 - feat: filtration of technology page (filtered technology's content now appears on screen)

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,14 +1,30 @@
-import { Link } from "react-router-dom"
-
+import { Link, useSearchParams } from "react-router-dom"
+import data from "../data.json"
 export default function Technology(){
+    const [searchParams] = useSearchParams()
+  
+    const technologyFilter = searchParams.get("technology") || "launchvehicle"
+    const technologyData = data.technology
+    console.log(technologyData)
+    const filteredTechnology = technologyData.filter(function(tech){
+        return technologyFilter === tech.name.replace(/\s+/g, "").toLowerCase() 
+    })
+    console.log(filteredTechnology)
+    //There is an issue with the filtering of "Space capsule"
     return (
         <div className = "tech-div">
             <p>03 Space Launch 101</p>
             <div className="tech-nav">
-                <Link to="/technology/launchvehicle" className="tech-link">1</Link>
-                <Link to="/technology/spaceport" className="tech-link">2</Link>
-                <Link to="/technology/capsule" className="tech-link">3</Link>
+                <Link to="?technology=launchvehicle" className="tech-link">1</Link>
+                <Link to="?technology=spaceport" className="tech-link">2</Link>
+                <Link to="?technology=spacecapsule" className="tech-link">3</Link>
             </div>
+            <div>
+                <h2 className="dark-gray-text">THE TERMINOLOGY...</h2>
+                <h1 className="white-text">{filteredTechnology[0].name.toUpperCase()}</h1>
+                <p className="light-blue-text">{filteredTechnology[0].description}</p>
+            </div>
+            <img src={`${filteredTechnology[0].images.portrait}`}/>
         </div>
     )
 }
